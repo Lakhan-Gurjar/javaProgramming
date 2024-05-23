527f8e30efee5abc15ce6bf0377142152fa6e979
Lakhan-Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2024-05-23 08:43:35 +0530

    conditionalStatements.java file added via git

---
+++ conditionalStatements.java
@@ -0,0 +1,148 @@
+// import java.util.*; // library for taking input
+
+public class conditionalStatements {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        /*
+         * int age = sc.nextInt();
+         * if ( age > 18) {
+         * System.out.println("Adult");
+         * } else {
+         * System.out.println("not adult");
+         * }
+         */
+
+        /*
+         * int number = sc.nextInt();
+         * if (number % 2 == 0) {
+         * System.out.println("Even number");
+         * } else {
+         * System.out.println("Odd number");
+         * }
+         */
+
+        /*
+         * int a = sc.nextInt();
+         * int b = sc.nextInt();
+         * if (a == b) {
+         * System.out.println("Equal");
+         * } else if (a > b) {
+         * System.out.println("a is greater");
+         * } else {
+         * System.out.println("a is lesser");
+         * }
+         */
+
+        /*  
+        int button = sc.nextInt();
+        switch (button) {
+            case 1:
+                System.out.println("Namaste");
+                break;
+            case 2:
+                System.out.println("Hello");
+                break;
+            case 3:
+                System.out.println("Bonjour");
+                break;
+
+            default:
+                System.out.println("Invalid button");
+                break;
+        }
+        */
+
+        /*
+            Homework Problems
+
+        Q1. Make a Calculator. Take 2 numbers(a & b) from the user and an operation as follows:
+            1 : + (Addition) a + b
+            2 : - (Subtraction) a - b
+            3 : * (Multiplication) a * b
+            4 : / (Division) a / b
+            5 : % (Modulo or remainder) a % b
+
+            Calculate the result according to the operation given and display it to the user.
+
+        Ans.int a = sc.nextInt();
+            int b = sc.nextInt();
+            int operator = sc.nextInt(); 
+            switch (operator) {
+                case 1 : 
+                    System.out.println(a + b);
+                    break;
+                case 2 : 
+                    System.out.println(a - b);
+                    break;
+                case 3 : 
+                    System.out.println(a * b);
+                    break;
+                case 4 : 
+                    if(b == 0) {
+                       System.out.println("Invalid Division");
+                   } else {
+                       System.out.println(a/b);
+                   }
+                    break;
+                case 5 : 
+                    if(b == 0) {
+                       System.out.println("Invalid Division");
+                   } else {
+                       System.out.println(a%b);
+                   }
+                    break;
+                default : 
+                    System.out.println("Invalid");
+            }
+        
+        Q2. Ask the user to enter the number of the month & print the name of the month. For eg - For '1' print 'January', '2' print 'February' & so on.
+
+        Ans.System.out.print("Enter the month number to be printed : ");
+            int month = sc.nextInt ();
+
+            switch (month) {
+                case 1 :
+                    System.out.println("January");
+                    break;
+                case 2 :
+                    System.out.println("February");
+                    break;
+                case 3 :
+                    System.out.println("March");
+                    break;
+                case 4 :
+                    System.out.println("April");
+                    break;
+                case 5 :
+                    System.out.println("May");
+                    break;
+                case 6 :
+                    System.out.println("June");
+                    break;
+                case 7 :
+                    System.out.println("July");
+                    break;
+                case 8 :
+                    System.out.println("August");
+                    break;
+                case 9 :
+                    System.out.println("September");
+                    break;
+                case 10 :
+                    System.out.println("October");
+                    break;
+                case 11 :
+                    System.out.println("November");
+                    break;
+                case 12 :
+                    System.out.println("December");
+                    break;
+                default :
+                    System.out.println("Enter valid month number.");
+                    break;
+            }
+            
+        */
+
+    }
+}
