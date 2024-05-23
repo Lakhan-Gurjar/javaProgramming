4f46e8deca5f742679a060d5f9e71d67bd3fe4ee
Lakhan-Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2024-05-23 08:42:33 +0530

    Variables.java file added via git

---
+++ Variables.java
@@ -0,0 +1,94 @@
+// import java.util.*; // library for taking input
+public class Variables{
+    public static void main(String[] args) {
+        // Output
+        // System.out.println("I Love Java");
+        
+        /*
+        Input
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next(); // Inputs only one token 
+        String name = sc.nextLine(); // accepts sentence 
+        int name = sc.nextInt(); // inputs integer value
+        float name = sc.nextFloat(); // inputs float value
+        double name = sc.nextDouble(); // inputs double value
+        long name = sc.nextLong(); // inputs long value
+        System.out.println(name);
+        */
+        
+
+        /* 
+        Variables
+
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+        int diff = b - a;
+        System.out.println(diff);
+        int product = a * b;
+        System.out.println(product);
+        float division = a / b;
+        System.out.println(division);
+        int ans = (a *b) / (a - b);
+        System.out.println(ans);
+        */
+        
+        
+    
+        /*
+        Question --> Take 2 variables 'a' & 'b' and print their sum.
+        
+        System.out.print("Enter value of number (a) : ");
+        // Scanner val = new Scanner(System.in);
+        int x = sc.nextInt();  
+        System.out.print("Enter value of number (b) : ");
+        int y = sc.nextInt();
+        
+        int sum = x + y;
+        System.out.print("Sum of a and b is : ");
+        System.out.println(sum);
+        */
+
+        /*
+        Homework Problems
+
+    Q1. Try to declare meaningful variables of each type. Eg - a variable named age should be a numeric type (int or float) not byte.
+    Ans.int age = 18;
+        float PI = 3.14F;
+        char fav = 'L';
+        double salary = 48.65;
+        String name = "Lakhan";
+        int Array[] = {23, 45};
+
+    Q2. Make a program that takes the radius of a circle as input, calculates its Circumference and area and prints it as output to the user.
+    Ans.System.out.print("Enter radius of circle : ");
+        Scanner sc = new Scanner(System.in);
+        float r = sc.nextFloat();
+        double Circumference = (2) * (3.14) * (r);
+        System.out.print("Circumference of circle is : ");
+        System.out.println(Circumference);
+        double area = (3.14) * r * r;
+        System.out.print("Area of circle is : ");
+        System.out.println(area);
+        
+    Q3. Make a program that prints the table of a number that is imput by the user.
+    Ans.System.out.print("Enter a number : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n * 1);
+        System.out.println(n * 2);
+        System.out.println(n * 3);
+        System.out.println(n * 4);
+        System.out.println(n * 5);
+        System.out.println(n * 6);
+        System.out.println(n * 7);
+        System.out.println(n * 8);
+        System.out.println(n * 9);
+        System.out.println(n * 10);
+        
+        */
+    
+    }
+}
