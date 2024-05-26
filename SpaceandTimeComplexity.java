be1a5ebea1378e0c30be4e53c451bad39973949d
Lakhan-Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2024-05-26 10:57:21 +0530

    SpaceandTimeComplexity.java file added

---
+++ SpaceandTimeComplexity.java
@@ -0,0 +1,30 @@
+public class SpaceandTimeComplexity {
+    public static void main(String[] args){
+        /*
+        TIME COMPLEXITY -> 
+        Time complexity of an algorithm quantifies the amount of time taken by an algorithm to run as a function of the 
+        length of the input. 
+
+        TYPES OF NOTATIONS -> 
+        1. O-notation : It is used to denote asymptotic upper bound. For a given function g(n), we denote it by O(g(n)). Pronounced as "big-oh of g of n".
+           It is also known as worst case time complexity as it denotes the upperbound in which the algorithm terminates. 
+
+        2. Ω-notation : It is used to denote asymptotic lower bound. For a given function g(n), we denote it by Ω(g(n)). Pronounced as "big-omega of g of n".
+           It is also known as best case time complexity as it denotes the lower bound in which the algorithm terminates.
+           
+        3. ϴ-notation : It is used to denote the average time of a program.
+
+        Comparison of functions on the basis of time complexity ->
+        It follows the following order in case of time complexity :
+        O(n^n) > O(n!) > O(n^3) > O(n^2) > O(n.log(n)) > O(n.log(log(n))) > O(n) > O(sqrt(n)) > O(log(n)) > O(1)
+
+        NOTE :- Reverse is the order for better performance of a code with corresponding time complexity, i.e. a program with less time complexity is more efficient.
+
+        SPACE COMPLEXITY ->
+        Space complexity of an algorithm quantifies the ammount of spac taken by a program to run as a function of length of the input. It is directly
+        proportional to the largest memory your program acquires at any instance during runtime.
+
+        For example : int consumes 4 bytes of memory
+        */
+    }
+}
