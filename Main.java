fa2012437e5a27330a2cc1a935e4d8b25ba45fd7
Lakhan Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2024-07-06 12:59:57 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lakhan Gurjar");
     }
 }
+/*
+public (in first line) :- public is an access modifier which allows to access the class from anywhere
+*/
